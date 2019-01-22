083e3ece2d94cfc45f199567dfc6c89aad7e2073
zhongyongbin <zhongyongbin@foxmail.com>
2019-01-22 14:06:04 +0800

    update

--- src/main/java/com/bz/gists/util/LogUtil.java
+++ src/main/java/com/bz/gists/util/LogUtil.java
@@ -20,16 +20,16 @@ public final class LogUtil {
 
     private static final String TIMESTAMP_KEY = "_timestamp";
 
-    private static final DateTimeFormatter DATA_LOG_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    private static final DateTimeFormatter LOG_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
     public static void dataLog(Logger logger, Object obj) {
         Map<String, Object> data = new LinkedHashMap<>();
-        data.put(TIMESTAMP_KEY, DATA_LOG_TIME_FORMATTER.format(LocalDateTime.now()));
+        data.put(TIMESTAMP_KEY, LOG_TIME_FORMATTER.format(LocalDateTime.now()));
         data.put(SOURCE_KEY, obj);
         logger.info(JsonUtil.toJson(data));
     }
 
     public static String formatTimestamp(long timestamp) {
-        return DATA_LOG_TIME_FORMATTER.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault()));
+        return LOG_TIME_FORMATTER.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault()));
     }
 }
