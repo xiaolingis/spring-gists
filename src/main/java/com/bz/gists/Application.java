79a80734a7313f3d0561fe62c31d15bb947114cc
zhongyongbin <zhongyongbin@foxmail.com>
2019-02-19 17:18:31 +0800

    更新获取本地IP方法

--- src/main/java/com/bz/gists/Application.java
+++ src/main/java/com/bz/gists/Application.java
@@ -10,8 +10,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.net.UnknownHostException;
-
 /**
  * Created on 2019/1/19
  *
@@ -24,7 +22,7 @@ public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-    public static void main(final String[] args) throws UnknownHostException {
+    public static void main(final String[] args) throws Exception {
         final SpringApplication app = new SpringApplication(Application.class);
         final Environment env = app.run(args).getEnvironment();
         ProfileUtil.addEnvironment(env);
@@ -32,15 +30,30 @@ public class Application {
         log();
     }
 
-    private static void log() throws UnknownHostException {
+    private static void log() throws Exception {
         StringBuilder log = new StringBuilder();
         log.append("\n----------------------------------------------------------\n\t");
         log.append("Running with spring profile : {}\n\t");
         log.append("Application '{}' is running! Access URLs:\n\t");
         log.append("Local: \t\thttp://127.0.0.1:{}\n\t");
-        log.append("External: \thttp://{}:{}");
+        log.append("External: \t{}");
         log.append("\n----------------------------------------------------------");
 
-        LOGGER.info(log.toString(), ProfileUtil.getActiveProfile(), ProfileUtil.getApplicationName(), ProfileUtil.getServerPort(), ProfileUtil.getHostAddress(), ProfileUtil.getServerPort());
+        StringBuilder externalAddress = new StringBuilder();
+        String[] hostAddresses = ProfileUtil.getHostAddresses();
+        for (int i = 0; i < hostAddresses.length; i++) {
+            if (i != 0) {
+                externalAddress.append("            ");
+            }
+            externalAddress.append(String.format("http://%s:%s", hostAddresses[i], ProfileUtil.getServerPort()));
+            if (i != hostAddresses.length - 1) {
+                externalAddress.append("\n\t");
+            }
+        }
+        LOGGER.info(log.toString(),
+                ProfileUtil.getActiveProfile(),
+                ProfileUtil.getApplicationName(),
+                ProfileUtil.getServerPort(),
+                externalAddress.toString());
     }
 }
