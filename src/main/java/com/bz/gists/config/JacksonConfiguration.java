a0b8951dc4fa733b3c6eafb7510c7ce52806bbd4
zhongyongbin <zhongyongbin@gomo.com>
2019-04-03 15:25:16 +0800

    更新配置

--- src/main/java/com/bz/gists/config/JacksonConfiguration.java
+++ src/main/java/com/bz/gists/config/JacksonConfiguration.java
@@ -1,39 +1,39 @@
-package com.bz.gists.config;
-
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-
-import org.springframework.boot.jackson.JsonComponent;
-
-import java.io.IOException;
-import java.util.Date;
-
-/**
- * Created at 2018/12/27
- *
- * @author zhongyongbin
- */
-@JsonComponent
-public class JacksonConfiguration {
-
-    public static class DateToLongSerializer extends JsonSerializer<Date> {
-        @Override
-        public void serialize(Date date, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-            gen.writeNumber(date.getTime());
-        }
-
-    }
-
-    public static class LongToDateDeserializer extends JsonDeserializer<Date> {
-
-        @Override
-        public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            return new Date(p.getValueAsLong());
-        }
-    }
-}
+//package com.bz.gists.config;
+//
+//import com.fasterxml.jackson.core.JsonGenerator;
+//import com.fasterxml.jackson.core.JsonParser;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.DeserializationContext;
+//import com.fasterxml.jackson.databind.JsonDeserializer;
+//import com.fasterxml.jackson.databind.JsonSerializer;
+//import com.fasterxml.jackson.databind.SerializerProvider;
+//
+//import org.springframework.boot.jackson.JsonComponent;
+//
+//import java.io.IOException;
+//import java.util.Date;
+//
+///**
+// * Created at 2018/12/27
+// *
+// * @author zhongyongbin
+// */
+//@JsonComponent
+//public class JacksonConfiguration {
+//
+//    public static class DateToLongSerializer extends JsonSerializer<Date> {
+//        @Override
+//        public void serialize(Date date, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+//            gen.writeNumber(date.getTime());
+//        }
+//
+//    }
+//
+//    public static class LongToDateDeserializer extends JsonDeserializer<Date> {
+//
+//        @Override
+//        public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+//            return new Date(p.getValueAsLong());
+//        }
+//    }
+//}
