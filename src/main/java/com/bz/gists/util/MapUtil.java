356c5ffeb640cc7506096d158597fefaadddcd76
zhongyongbin <zhongyongbin@gomo.com>
2019-05-23 17:41:20 +0800

    添加工具

---
+++ src/main/java/com/bz/gists/util/MapUtil.java
@@ -0,0 +1,26 @@
+package com.bz.gists.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created on 2019/5/23
+ *
+ * @author zhongyongbin
+ */
+public final class MapUtil {
+
+    public static Map<String, String> toStringMap(Map<?, ?> map) {
+        Map<String, String> stringMap = new HashMap<>();
+        map.forEach((k, v) -> stringMap.put(String.valueOf(k), String.valueOf(v)));
+
+        return stringMap;
+    }
+
+    public static Map<String, Object> toStringKeyMap(Map<?, ?> map) {
+        Map<String, Object> stringKeyMap = new HashMap<>();
+        map.forEach((k, v) -> stringKeyMap.put(String.valueOf(k), v));
+
+        return stringKeyMap;
+    }
+}
