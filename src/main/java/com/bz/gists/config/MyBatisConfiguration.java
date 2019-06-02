24b8d8eba16e0d40c95229e46fe86e56e1b0c01f
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-02 22:48:44 +0800

    优化 MyBatis 配置

--- src/main/java/com/bz/gists/config/MyBatisConfiguration.java
+++ src/main/java/com/bz/gists/config/MyBatisConfiguration.java
@@ -2,18 +2,22 @@ package com.bz.gists.config;
 
 import com.github.pagehelper.PageInterceptor;
 
-import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Created on 2019/6/2
  *
  * @author zhongyongbin
  */
+@MapperScan("com.bz.gists")
 @Configuration
-public class MyBatisConfiguration implements ConfigurationCustomizer {
-    @Override
-    public void customize(org.apache.ibatis.session.Configuration configuration) {
-        configuration.addInterceptor(new PageInterceptor());
+public class MyBatisConfiguration {
+
+    @Bean
+    public PageInterceptor pageInterceptor() {
+        return new PageInterceptor();
     }
 }
