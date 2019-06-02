5d0785695e87ba41652ad4642ec2281723f65d22
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-02 22:49:03 +0800

    update

--- src/main/java/com/bz/gists/config/MyBatisConfiguration.java
+++ src/main/java/com/bz/gists/config/MyBatisConfiguration.java
@@ -5,14 +5,11 @@ import com.github.pagehelper.PageInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import tk.mybatis.spring.annotation.MapperScan;
-
 /**
  * Created on 2019/6/2
  *
  * @author zhongyongbin
  */
-@MapperScan("com.bz.gists")
 @Configuration
 public class MyBatisConfiguration {
 
