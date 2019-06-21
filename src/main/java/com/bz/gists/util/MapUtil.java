55ea95e818b11e9853180c4b6869bdbc71ccb480
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-21 23:08:19 +0800

    优化

--- src/main/java/com/bz/gists/util/MapUtil.java
+++ src/main/java/com/bz/gists/util/MapUtil.java
@@ -11,14 +11,14 @@ import java.util.Map;
 public final class MapUtil {
 
     public static Map<String, String> toStringMap(Map<?, ?> map) {
-        Map<String, String> stringMap = new HashMap<>();
+        Map<String, String> stringMap = new HashMap<>(map.size());
         map.forEach((k, v) -> stringMap.put(String.valueOf(k), String.valueOf(v)));
 
         return stringMap;
     }
 
     public static Map<String, Object> toStringKeyMap(Map<?, ?> map) {
-        Map<String, Object> stringKeyMap = new HashMap<>();
+        Map<String, Object> stringKeyMap = new HashMap<>(map.size());
         map.forEach((k, v) -> stringKeyMap.put(String.valueOf(k), v));
 
         return stringKeyMap;
