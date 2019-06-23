b9f81c9a05030352660d26752d6baa21fd8e2e40
zhongyongbin <zhongyongbin@foxmail.com>
2019-06-23 17:42:00 +0800

    优化工具类

--- src/main/java/com/bz/gists/cron/Scheduler.java
+++ src/main/java/com/bz/gists/cron/Scheduler.java
@@ -1,9 +1,10 @@
 package com.bz.gists.cron;
 
-import com.bz.gists.util.SpringUtil;
-
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Executor;
@@ -16,13 +17,20 @@ import java.util.concurrent.Executor;
  * @author zhongyongbin
  */
 @Service
-public final class Scheduler {
+public final class Scheduler implements ApplicationContextAware {
 
     @Autowired
     @Qualifier("serviceExecutor")
     private Executor serviceExecutor;
 
+    private ApplicationContext applicationContext;
+
     private void executeCrontab(Class<? extends BaseCrontab> crontab) {
-        serviceExecutor.execute(() -> SpringUtil.getBean(crontab).execute());
+        serviceExecutor.execute(() -> applicationContext.getBean(crontab).execute());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
