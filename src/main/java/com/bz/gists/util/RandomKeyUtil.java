1f0bc99cc4b0a428f83aba9893fa9911f16e7bb0
zhongyongbin <zhongyongbin@foxmail.com>
2019-07-04 20:42:17 +0800

    update

--- src/main/java/com/bz/gists/util/RandomKeyUtil.java
+++ src/main/java/com/bz/gists/util/RandomKeyUtil.java
@@ -14,27 +14,27 @@ public final class RandomKeyUtil {
             "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
             "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
-    private static final Long LEFT = 63176045467600L;
+    private static final Long RANDOM_ORIGIN = 63176045467600L;
 
-    private static final Long RIGHT = 218340105584895L;
+    private static final Long RANDOM_BOUND = 218340105584895L;
 
     private RandomKeyUtil() {
     }
 
     /**
-     * 随机生成密钥，由数字，小写字母，大写字母构成
+     * 生成随机密钥，由数字，小写字母，大写字母构成
      *
      * @param length 密钥长度，最大不超过 1024
      * @return 随机密钥
      */
-    public static String getRandomKey(int length) {
+    public static String generateKey(int length) {
         if (length > 1024) {
             throw new IllegalArgumentException("length too large, the max large is 1024");
         }
 
         StringBuilder key = new StringBuilder();
         while (key.length() < length) {
-            long count = ThreadLocalRandom.current().nextLong(LEFT, RIGHT);
+            long count = ThreadLocalRandom.current().nextLong(RANDOM_ORIGIN, RANDOM_BOUND);
             while (count > 0 && key.length() + 1 <= length) {
                 long mod = count % 62L;
                 count /= 62L;
