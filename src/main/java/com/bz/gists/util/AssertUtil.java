2c84652c5c1ac5a154a41e18c007eff6cb81b83f
zhongyongbin <zhongyongbin@gomo.com>
2019-11-05 17:52:17 +0800

    update

--- src/main/java/com/bz/gists/util/AssertUtil.java
+++ src/main/java/com/bz/gists/util/AssertUtil.java
@@ -13,55 +13,55 @@ public final class AssertUtil {
     private AssertUtil() {
     }
 
-    public static <X extends Throwable> void isTrue(boolean bool, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireTrue(boolean bool, Supplier<X> supplier) throws X {
         if (!bool) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void nonNull(Object object, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNonNull(Object object, Supplier<X> supplier) throws X {
         if (Objects.isNull(object)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void numberInRange(int number, int leftOpenInterval, int rightOpenInterval, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNumberInRange(int number, int leftOpenInterval, int rightOpenInterval, Supplier<X> supplier) throws X {
         if (!(number > leftOpenInterval && number < rightOpenInterval)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void numberGtRange(int number, int leftOpenInterval, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNumberGtRange(int number, int leftOpenInterval, Supplier<X> supplier) throws X {
         if (!(number > leftOpenInterval)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void numberLtRange(int number, int rightOpenInterval, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNumberLtRange(int number, int rightOpenInterval, Supplier<X> supplier) throws X {
         if (!(number < rightOpenInterval)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void numberInRange(long number, long leftOpenInterval, long rightOpenInterval, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNumberInRange(long number, long leftOpenInterval, long rightOpenInterval, Supplier<X> supplier) throws X {
         if (!(number > leftOpenInterval && number < rightOpenInterval)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void numberGtRange(long number, long leftOpenInterval, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNumberGtRange(long number, long leftOpenInterval, Supplier<X> supplier) throws X {
         if (!(number > leftOpenInterval)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void numberLtRange(long number, long rightOpenInterval, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNumberLtRange(long number, long rightOpenInterval, Supplier<X> supplier) throws X {
         if (!(number < rightOpenInterval)) {
             throw supplier.get();
         }
     }
 
-    public static <X extends Throwable> void hasLength(String string, Supplier<X> supplier) throws X {
+    public static <X extends Throwable> void requireNotBlank(String string, Supplier<X> supplier) throws X {
         if (!(Objects.nonNull(string) && string.length() > 0)) {
             throw supplier.get();
         }
